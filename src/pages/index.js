f26506ffa7db957cff6b91a3d346c236210ed8b7
Zerio <54480523+Z3rio@users.noreply.github.com>
2023-04-06 19:27:34 +0200

    Add redirect to default page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+// import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { Redirect } from "@docusaurus/router";
 
 import styles from "./index.module.css";
 
@@ -14,26 +15,11 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  return (
-    <Layout title="Home" description="Homepage for Zerio-Scripts documentation">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/docs/start/introduction" />;
 }
