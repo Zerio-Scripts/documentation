a182231f50f09d3184a41c9ea1f080256640cfe2
Z3rio <neoboij@gmail.com>
2024-11-06 14:37:04 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@ import nextra from "nextra";
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+  themeConfig: "./theme.config.tsx"
 });
 
 export default withNextra({
   i18n: {
     locales: ["en"],
     defaultLocale: "en"
-  },
+  }
 });
