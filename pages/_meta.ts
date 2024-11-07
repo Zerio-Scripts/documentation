47f76b42e462694c7dfc085e434505fee24efb0a
Z3rio <neoboij@gmail.com>
2024-11-07 12:18:08 +0100

    feat: resource structure

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -3,5 +3,15 @@ export default {
   beforebuying: "Before buying",
   afterbuying: "After buying",
   updateresource: "Update resource",
-  commonissues: "Common issues"
+  commonissues: "Common issues",
+  radio: "Zerio Radio",
+  cardealer: "Zerio Cardealer",
+  bossmenu: "Zerio Bossmenu",
+  invoice: "Zerio Invoice",
+  parking: "Zerio Parking",
+  dispatch: "Zerio Dispatch",
+  multijobs: "Zerio Multijobs",
+  crafting: "Zerio Crafting",
+  garage: "Zerio Garage",
+  characters: "Zerio Characters"
 };
