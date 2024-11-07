32778701d40b2204bc886aeb5eb43f06d12febcd
Z3rio <neoboij@gmail.com>
2024-11-07 10:41:52 +0100

    chore: zerio-scripts branding

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,8 +13,24 @@ const config: DocsThemeConfig = {
     link: "http://discord.zerio-scripts.com"
   },
   docsRepositoryBase: "https://github.com/Zerio-Scripts/documentation",
+  logo: (
+    <>
+      <img src="/static/logo.png" width="32" height="32" />
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Zerio-Scripts
+      </span>
+    </>
+  ),
   footer: {
-    content: <span>Zerip-Scripts Documentation</span>
+    content: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://store.zerio-scripts.com" target="_blank">
+          Zerio-Scripts
+        </a>
+        .
+      </span>
+    )
   }
 };
 
