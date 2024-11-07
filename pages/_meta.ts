98c97f728f21dbaccdf6dac2585e1bfb658e7dd0
Z3rio <neoboij@gmail.com>
2024-11-07 10:17:49 +0100

    feat: start adding documentation

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,15 +1,7 @@
 export default {
   index: "Introduction",
-  another: "Another Page",
-  advanced: "Advanced (A Folder)",
-  about: {
-    title: "About",
-    type: "page"
-  },
-  contact: {
-    title: "Contact ↗",
-    type: "page",
-    href: "https://twitter.com/shuding_",
-    newWindow: true
-  }
+  beforebuying: "Before buying",
+  afterbuying: "After buying",
+  updateresource: "Update resource",
+  commonissues: "Common issues"
 };
---
+++ pages/commonissues/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  lackingentitlement: "Lacking entitlement",
+  syntaxerror: "Syntax error near <\\1>",
+  brokenuifiles: "Broken UI files",
+  otherbugs: "Found a different bug?"
+};
