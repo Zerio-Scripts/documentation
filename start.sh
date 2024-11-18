28e033453e69d21fe2a44ed30f7fd135d87e2510
Zerio <neoboij@gmail.com>
2024-11-18 23:03:29 +0100

    fix: import pnpm in `start.sh`

--- start.sh
+++ start.sh
@@ -5,4 +5,12 @@ export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 # end of nvm
 
+# pnpm
+export PNPM_HOME="/home/ubuntu/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
 pnpm start
